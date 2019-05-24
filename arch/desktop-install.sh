1961b7124289c12e6bef063a71e7f39771044268
blah42 <stevencoulter1808@yahoo.com>
2019-05-24 03:23:58 -0400

    summary

--- arch/desktop-install.sh
+++ arch/desktop-install.sh
@@ -7,19 +7,9 @@ swapon /dev/sdb1
 mkdir /mnt/home
 mkdir /mnt/usr
 mount /dev/sdb2 /mnt
+pacstrap /mnt base base-devel
 mount /dev/sda2 /mnt/home
 mount /dev/sda1 /mnt/usr
 pacstrap /mnt base base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-adduser steven
-passwd steven
-echo steven-PC > /etc/hostname
-pacman -S --noconfirm dialog wpa_supplicant
-pacman -S --noconfirm grub
-grub-install /dev/sdb
-grub-mkconfig -o /boot/grub/grub.cfg
-pacman -Syu --noconfirm
-pacman -S --noconfirm i3-gaps lightdm xorg-server lightdm-gtk-greeter
-systemctl enable lightdm.service
-psswd
